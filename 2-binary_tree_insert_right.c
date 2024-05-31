fd7d99eaa00c01d3c9ef329962b22a5a4f368493
Roslyn10 <roslynclewis1004@gmail.com>
2024-05-31 15:07:19 +0200

    Tryng to add the memory after null

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -13,12 +13,12 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	new_node = malloc(sizeof(binary_tree_t));
-
 	if (parent == NULL || new_node == NULL)
 	{
 		return (NULL);
 	}
+
+	new_node = malloc(sizeof(binary_tree_t));
 
 	new_node->n = value;
 	new_node->parent = parent;
