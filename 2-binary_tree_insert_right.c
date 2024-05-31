78ad6c4207cb124cfc0596bb0bc9465c1dcd3e7e
Roslyn10 <roslynclewis1004@gmail.com>
2024-05-31 15:00:01 +0200

    Putting it back

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -13,6 +13,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
+	new_node = malloc(sizeof(binary_tree_t));
 
 	if (parent == NULL || new_node == NULL)
 	{
