09fcb39c3f90ca41c2901f1de321cedfff4d2c98
Roslyn10 <roslynclewis1004@gmail.com>
2024-06-04 11:27:54 +0200

    Trying to fix the memory error

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - A function that goes through a binary tree
+ * Description - Goes through a binary tree using pre-order traversal
+ * @tree: A pointer to the root node of the tree to traverse
+ * @func: A pointer to a function to call for each node
+ * Return: Nothing
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+{
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+
+
