814142b884ebe5ede3d7ae82c3fbc05208d93b4d
Roslyn10 <roslynclewis1004@gmail.com>
2024-06-04 11:40:08 +0200

    Adding the code for pre-order

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -8,11 +8,14 @@
  * Return: Nothing
  */
 
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 	{
 		return;
 	}
 
-
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
