622604648bfaa0f5f36093352a97f4266941d160
Roslyn10 <roslynclewis1004@gmail.com>
2024-06-10 13:29:56 +0200

    Fixing the code for 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -17,8 +17,8 @@ size_t tree_height(const binary_tree_t *tree)
 	}
 	else
 	{
-		left_height = tree->left ? 1 + tree_height(tree->left) : 0;
-		right_height = tree->right ? 1 + tree_height(tree->right) : 0;
+		left_height = tree->left ? 1 + tree_height(tree->left) : 1;
+		right_height = tree->right ? 1 + tree_height(tree->right) : 1;
 		return ((left_height > right_height) ? left_height : right_height);
 	}
 }
